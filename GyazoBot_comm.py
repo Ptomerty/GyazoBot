da1e2c305db22911d58a1aade08ce35d032da101
Ptomerty <shua3.14@gmail.com>
2017-06-02 20:23:31 -0400

    ignoreme updated for posts and comments

--- GyazoBot_comm.py
+++ GyazoBot_comm.py
@@ -3,6 +3,9 @@ import requests
 import re
 import os
 
+ignore = []
+mtime = 0
+
 
 def check_url(url):
     try:
@@ -28,35 +31,44 @@ def process(url):
         return ''
 
 
+def refreshIgnore():
+    global ignore
+    global mtime
+    if os.path.isfile("./ignore") and os.path.getmtime("./ignore") != mtime:
+        with open("./ignore", "r") as f:
+            for line in f:
+                item = line.split("\n")[0]
+                if item not in ignore:
+                    ignore.append(item)
+        mtime = os.path.getmtime("./ignore")
+
+
 def main():
     reddit = praw.Reddit('GyazoBot', user_agent='GyazoBot by derpherp128')
 
     regex = '(?<!\w\.)gyazo\.com/\w{32}'
 
     reply_template_header = ('Hi, I\'m a bot that links Gyazo images directly.\n\n')
-
     reply_template_footer = ('^^[Source](https://github.com/Ptomerty/GyazoBot) ^^| '
                              '^^[Why?](https://github.com/Ptomerty/GyazoBot/blob/master/README.md) ^^| '
                              '^^[Creator](https://np.reddit.com/u/derpherp128) ^^| '
-                             '^^[leave me alone](https://np.reddit.com/message/compose/?to=Gyazo_Bot'
+                             '^^[leavemealone](https://np.reddit.com/message/compose/?to=Gyazo_Bot'
                              '&subject=ignoreme&message=ignoreme)')
-    ignore = []
+    global ignore
     comments = []
 
-    if os.path.isfile("./ignore"):
-        with open("./ignore", "r") as f:
-            for line in f:
-                ignore.append(line.split("\n")[0])
+    refreshIgnore()
 
     if os.path.isfile("./comments"):
         with open("./comments", "r") as f:
             for line in f:
                 comments.append(line.split("\n")[0])
 
-    for comment in reddit.subreddit('ZXhwb3NlX2tyZXNfYmFv').stream.comments():
+    for comment in reddit.subreddit('all').stream.comments():
+        refreshIgnore()
         if comment.author not in ignore and comment.id not in comments:
             list = re.findall(regex, comment.body)
-            if (list):
+            if list:
                 a = reply_template_header
                 for url in list:
                     fixed = process(url)
